f4dcd24e786a58f2dd232d8bdc1b9605e8b836a5
Tom Hilton <tom.hilton@ajbell.co.uk>
2024-03-25 14:01:59 +0000

    Migrate to Vue 3.1 with compat mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
-import vue2 from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2,
+          },
+        },
+      },
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // comment out the line below and the tests pass
+      vue: '@vue/compat',
     },
   },
   test: {
